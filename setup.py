4958bc4d317317a88cde9f59f31672ba4ed9ded7
Andrei Gherzan <andrei.gherzan@windriver.com>
2013-12-23 23:00:35 +0200

    Initial version: 1.0a

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='notifmail',
+      version='1.0a',
+      scripts=['notifmail'],
+      description='IMAP email notifier',
+      author='Andrei Gherzan',
+      author_email='andrei@gherzan.ro',
+      url='https://github.com/agherzan/notifmail',
+      license='MIT',
+     )
